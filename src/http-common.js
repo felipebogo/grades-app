ca51b1f400c4598c2b96ed3e8942baa7da998e9f
Felipe Bogo <felipe.bogo@jupiter.co.ao>
2020-07-05 05:25:17 +0100

    deploy

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: process.env.SERVER_URL /* || 'http://localhost:3333/' */,
+  baseURL: process.env.REACT_APP_SERVER_URL || 'http://localhost:3333/',
   headers: {
     'Content-type': 'application/json',
   },
